152d2d3bb42868111a8793f553f3940b1d860166
Ifeoluwa Okesola <atoyebiife@gmail.com>
2025-09-19 13:39:59 +0000

    ci: use S3 backend with DynamoDB locking (compat fix)

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket        = "innovatemart-bedrock-tfstate-1758197320"
-    key           = "project-bedrock/infra/terraform.tfstate"
-    region        = "eu-west-1"
-    use_lockfile  = true
+    bucket         = "innovatemart-bedrock-tfstate-1758197320"
+    key            = "project-bedrock/infra/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "innovatemart-bedrock-tflocks" # CI-safe locking
+    encrypt        = true
   }
 }
